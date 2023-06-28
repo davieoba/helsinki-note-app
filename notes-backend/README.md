512e55345656935fcac09773898d6ac9fd4622f1
davieoba <bodunrindavidbond@gmail.com>
2023-06-28 22:17:29 +0100

    feat: run the build and update the frontend api endpoint

---
+++ notes-backend/README.md
@@ -0,0 +1,7 @@
+https://render-test-notes-backend.onrender.com
+
+
+after I build my frontend and I use express to serve it my url link would be
+https://render-test-notes-backend.onrender.com/index.html
+
+and it will serve my react app (build)
--- part2-notes/src/services/notes.js
+++ part2-notes/src/services/notes.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+// the base url is pointing to the backend api endpoint
+// development => http://localhost:3001/api/notes
+// production => https://render-test-notes-backend.onrender.com
+
+/**
+ * now since I am building the react app and serving it in my backend application, both development and production will be on the same api port so I can remove the hostname and protocol and make use of a relative path
+ */
+
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
