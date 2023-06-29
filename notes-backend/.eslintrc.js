9b663a9853da9b5f7b2ee87588148762db534d2f
davieoba <bodunrindavidbond@gmail.com>
2023-06-29 09:46:21 +0100

    feat: add eslint configuration

---
+++ notes-backend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'eqeqeq': 'error',
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
